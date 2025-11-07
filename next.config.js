61f87abf5fae090aa85c9b4007031012a424f51a
Gian Luca <48770103+G14nLuca@users.noreply.github.com>
2025-11-07 18:03:09 +0000

    "next.config.js" file added

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    useDeploymentId: true,
+    useDeploymentIdServerActions: true,
+  },
+};
+ 
+module.exports = nextConfig;
